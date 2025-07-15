4d4143f5c4b15a645e33ff40058fa0ddd055c5ed
Daniel Chae <daniel.jiwoong@gmail.com>
2025-07-15 11:15:29 +0900

    move import to top

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from .config import config
 from .services.embed_service import EmbedService
 from .services.vector_db_service import VectorDBService
@@ -28,8 +28,6 @@ def create_app(config_name=None):
     # Route for video ingest tasks
     @app.route("/tasks", methods=("GET", "POST"))
     def tasks():
-        from flask import request, jsonify
-
         if request.method == "GET":
             data = {
                 "tasks": [{"id": "1234"}],
