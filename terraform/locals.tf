70fc62a2c46b475397e866ef8a924ebb0b7ae4b2
Daniel Chae <daniel.jiwoong@gmail.com>
2025-07-31 04:31:02 +0900

    chore: make secret name consistent to "kubrick_secret"

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
   region          = var.aws_region
   env             = "dev"
-  secrets         = jsondecode(data.aws_secretsmanager_secret_version.kubrick_secrets_version.secret_string)
+  secret          = jsondecode(data.aws_secretsmanager_secret_version.kubrick_secret_version.secret_string)
   embedding_model = "Marengo-retrieval-2.7"
   clip_length     = 6
   min_similarity  = 0.2
