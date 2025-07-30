66222f4198cce117e9147b25fe99559ac5e78de6
Daniel Chae <daniel.jiwoong@gmail.com>
2025-07-31 03:06:52 +0900

    chore: moved aws region to tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,13 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-2"
+}
+
 variable "secrets_manager_name" {
   description = "The name of the AWS Secrets Manager secret containing API keys and RDS credentials"
   type        = string
-  default     = "kubrick_secret"
+  default     = "kubrick_secrets"
   # default     = "KubrickEmbeddingTaskConsumerSecret" ## Changed to try to make testing work, delete this
 }
+
