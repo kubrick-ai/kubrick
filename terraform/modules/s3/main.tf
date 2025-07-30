f3f924559c812704df7957ea120b64f20e778cc5
Dominique Morales Perez <dmp@Dominiques-MacBook-Pro.local>
2025-07-30 15:06:15 -0700

    FIX: Bucket Key is Enabled to fix presigned url bug

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -14,9 +14,22 @@ resource "aws_s3_bucket_public_access_block" "kubrick_video_upload_bucket" {
   bucket = aws_s3_bucket.kubrick_video_upload_bucket.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+# Default Encryption: Bucket Key = Enabled
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.kubrick_video_upload_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+
+    bucket_key_enabled = true
+  }
 }
 
 
