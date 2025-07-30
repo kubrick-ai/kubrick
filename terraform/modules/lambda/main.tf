5bfea648682382fae312ede8b3614af70f5a0b9a
Daniel Chae <daniel.jiwoong@gmail.com>
2025-07-31 02:03:33 +0900

    feat: lambda build automation

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,3 +1,173 @@
+# Build automation for Lambda layers
+resource "null_resource" "layer_build_vectordb" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/layers/vector_database_layer/vector_db_service.py")
+    deps_hash      = filemd5("${local.base_path}/layers/vector_database_layer/pyproject.toml")
+    package_exists = fileexists("${local.base_path}/layers/vector_database_layer/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/layers/vector_database_layer"
+  }
+}
+
+resource "null_resource" "layer_build_embed" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/layers/embed_service_layer/embed_service.py")
+    deps_hash      = filemd5("${local.base_path}/layers/embed_service_layer/pyproject.toml")
+    package_exists = fileexists("${local.base_path}/layers/embed_service_layer/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/layers/embed_service_layer"
+  }
+}
+
+resource "null_resource" "layer_build_config" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/layers/config_layer/config.py")
+    deps_hash      = filemd5("${local.base_path}/layers/config_layer/pyproject.toml")
+    package_exists = fileexists("${local.base_path}/layers/config_layer/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/layers/config_layer"
+  }
+}
+
+resource "null_resource" "layer_build_utils" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/layers/response_utils_layer/response_utils.py")
+    deps_hash      = filemd5("${local.base_path}/layers/response_utils_layer/pyproject.toml")
+    package_exists = fileexists("${local.base_path}/layers/response_utils_layer/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/layers/response_utils_layer"
+  }
+}
+
+# Build automation for Lambda functions
+resource "null_resource" "lambda_build_db_bootstrap" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/db_bootstrap/lambda_function.py")
+    config_hash    = filemd5("${local.base_path}/db_bootstrap/config.json")
+    deps_hash      = filemd5("${local.base_path}/db_bootstrap/pyproject.toml")
+    schema_hash    = filemd5("${local.base_path}/db_bootstrap/schema.sql")
+    package_exists = fileexists("${local.base_path}/db_bootstrap/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/db_bootstrap"
+  }
+}
+
+resource "null_resource" "lambda_build_api_search_handler" {
+  triggers = {
+    source_hash     = filemd5("${local.base_path}/api_search_handler/lambda_function.py")
+    controller_hash = filemd5("${local.base_path}/api_search_handler/search_controller.py")
+    errors_hash     = filemd5("${local.base_path}/api_search_handler/search_errors.py")
+    config_hash     = filemd5("${local.base_path}/api_search_handler/config.json")
+    deps_hash       = filemd5("${local.base_path}/api_search_handler/pyproject.toml")
+    package_exists  = fileexists("${local.base_path}/api_search_handler/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/api_search_handler"
+  }
+}
+
+resource "null_resource" "lambda_build_s3_delete_handler" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/s3_delete_handler/lambda_function.py")
+    utils_hash     = filemd5("${local.base_path}/s3_delete_handler/utils.py")
+    config_hash    = filemd5("${local.base_path}/s3_delete_handler/config.json")
+    deps_hash      = filemd5("${local.base_path}/s3_delete_handler/pyproject.toml")
+    package_exists = fileexists("${local.base_path}/s3_delete_handler/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/s3_delete_handler"
+  }
+}
+
+resource "null_resource" "lambda_build_api_fetch_videos_handler" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/api_fetch_videos_handler/lambda_function.py")
+    config_hash    = filemd5("${local.base_path}/api_fetch_videos_handler/config.json")
+    deps_hash      = filemd5("${local.base_path}/api_fetch_videos_handler/pyproject.toml")
+    package_exists = fileexists("${local.base_path}/api_fetch_videos_handler/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/api_fetch_videos_handler"
+  }
+}
+
+resource "null_resource" "lambda_build_api_video_upload_link_handler" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/api_video_upload_link_handler/lambda_function.py")
+    deps_hash      = filemd5("${local.base_path}/api_video_upload_link_handler/pyproject.toml")
+    package_exists = fileexists("${local.base_path}/api_video_upload_link_handler/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/api_video_upload_link_handler"
+  }
+}
+
+resource "null_resource" "lambda_build_api_fetch_tasks_handler" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/api_fetch_tasks_handler/lambda_function.py")
+    config_hash    = filemd5("${local.base_path}/api_fetch_tasks_handler/config.json")
+    deps_hash      = filemd5("${local.base_path}/api_fetch_tasks_handler/pyproject.toml")
+    package_exists = fileexists("${local.base_path}/api_fetch_tasks_handler/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/api_fetch_tasks_handler"
+  }
+}
+
+resource "null_resource" "lambda_build_sqs_embedding_task_producer" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/sqs_embedding_task_producer/lambda_function.py")
+    utils_hash     = filemd5("${local.base_path}/sqs_embedding_task_producer/utils.py")
+    config_hash    = filemd5("${local.base_path}/sqs_embedding_task_producer/config.json")
+    deps_hash      = filemd5("${local.base_path}/sqs_embedding_task_producer/pyproject.toml")
+    package_exists = fileexists("${local.base_path}/sqs_embedding_task_producer/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/sqs_embedding_task_producer"
+  }
+}
+
+resource "null_resource" "lambda_build_sqs_embedding_task_consumer" {
+  triggers = {
+    source_hash    = filemd5("${local.base_path}/sqs_embedding_task_consumer/lambda_function.py")
+    config_hash    = filemd5("${local.base_path}/sqs_embedding_task_consumer/config.json")
+    deps_hash      = filemd5("${local.base_path}/sqs_embedding_task_consumer/pyproject.toml")
+    package_exists = fileexists("${local.base_path}/sqs_embedding_task_consumer/package.zip")
+  }
+
+  provisioner "local-exec" {
+    command     = "./build-package.sh"
+    working_dir = "${local.base_path}/sqs_embedding_task_consumer"
+  }
+}
+
 # Lambda Layers
 resource "aws_lambda_layer_version" "vectordb_layer" {
   layer_name               = "vectordb_layer"
@@ -5,6 +175,8 @@ resource "aws_lambda_layer_version" "vectordb_layer" {
   compatible_architectures = ["x86_64"]
   filename                 = "${local.base_path}/layers/vector_database_layer/package.zip"
   description              = "Module for interacting with the PostgreSQL vector database"
+
+  depends_on = [null_resource.layer_build_vectordb]
 }
 
 resource "aws_lambda_layer_version" "embed_layer" {
@@ -13,6 +185,8 @@ resource "aws_lambda_layer_version" "embed_layer" {
   compatible_architectures = ["x86_64"]
   filename                 = "${local.base_path}/layers/embed_service_layer/package.zip"
   description              = "Multi-modal embedding extraction and management module using TwelveLabs API."
+
+  depends_on = [null_resource.layer_build_embed]
 }
 
 resource "aws_lambda_layer_version" "config_layer" {
@@ -21,6 +195,8 @@ resource "aws_lambda_layer_version" "config_layer" {
   compatible_architectures = ["x86_64"]
   filename                 = "${local.base_path}/layers/config_layer/package.zip"
   description              = "Configuration management module for secrets and database settings."
+
+  depends_on = [null_resource.layer_build_config]
 }
 
 resource "aws_lambda_layer_version" "utils_layer" {
@@ -29,6 +205,8 @@ resource "aws_lambda_layer_version" "utils_layer" {
   compatible_architectures = ["x86_64"]
   filename                 = "${local.base_path}/layers/response_utils_layer/package.zip"
   description              = "Utility module for error handling, S3 presigned URL generation, and API response construction."
+
+  depends_on = [null_resource.layer_build_utils]
 }
 
 # Security Group
@@ -72,6 +250,8 @@ resource "aws_lambda_function" "kubrick_db_bootstrap" {
   }
 
   timeout = 300 # 5 minutes timeout
+
+  depends_on = [null_resource.lambda_build_db_bootstrap]
 }
 
 resource "null_resource" "invoke_db_bootstrap" {
@@ -125,6 +305,8 @@ resource "aws_lambda_function" "kubrick_api_search_handler" {
   }
 
   timeout = 900 # 15 minutes timeout
+
+  depends_on = [null_resource.lambda_build_api_search_handler]
 }
 
 # kubrick_s3_delete_handler
@@ -152,6 +334,8 @@ resource "aws_lambda_function" "kubrick_s3_delete_handler" {
   }
 
   timeout = 900 # 15 minutes timeout
+
+  depends_on = [null_resource.lambda_build_s3_delete_handler]
 }
 
 # kubrick_api_fetch_videos_handler
@@ -181,6 +365,8 @@ resource "aws_lambda_function" "kubrick_api_fetch_videos_handler" {
   }
 
   timeout = 900 # 15 minutes timeout
+
+  depends_on = [null_resource.lambda_build_api_fetch_videos_handler]
 }
 
 # kubrick_api_video_upload_link_handler
@@ -208,6 +394,8 @@ resource "aws_lambda_function" "kubrick_api_video_upload_link_handler" {
   }
 
   timeout = 900 # 15 minutes timeout
+
+  depends_on = [null_resource.lambda_build_api_video_upload_link_handler]
 }
 
 # kubrick_api_fetch_tasks_handler
@@ -236,6 +424,8 @@ resource "aws_lambda_function" "kubrick_api_fetch_tasks_handler" {
   }
 
   timeout = 900 # 15 minutes timeout
+
+  depends_on = [null_resource.lambda_build_api_fetch_tasks_handler]
 }
 
 # kubrick_sqs_embedding_task_producer
@@ -268,6 +458,8 @@ resource "aws_lambda_function" "kubrick_sqs_embedding_task_producer" {
   }
 
   timeout = 900 # 15 minutes timeout
+
+  depends_on = [null_resource.lambda_build_sqs_embedding_task_producer]
 }
 
 # kubrick_sqs_embedding_task_consumer
@@ -298,4 +490,6 @@ resource "aws_lambda_function" "kubrick_sqs_embedding_task_consumer" {
   }
 
   timeout = 900 # 15 minutes timeout
+
+  depends_on = [null_resource.lambda_build_sqs_embedding_task_consumer]
 }
