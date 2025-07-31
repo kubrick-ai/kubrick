b898cbdccbcc9d927b90660f9a1001d92117ccbc
Daniel Chae <daniel.jiwoong@gmail.com>
2025-07-31 17:28:33 +0900

    FIX: ONLY THE LAMBDA YOU MODIFY IS REBUILT

--- terraform/modules/lambda/archives.tf
+++ terraform/modules/lambda/archives.tf
@@ -1,123 +1,260 @@
-locals {
-  lambda_configs = {
-    # Lambda Layers
-    config_layer = {
-      path = "layers/config_layer"
-      sources = {
-        source_hash = "config.py"
-      }
-      source_dir = "package"
-    }
-    embed_layer = {
-      path = "layers/embed_service_layer"
-      sources = {
-        source_hash = "embed_service.py"
-      }
-      source_dir = "package"
-    }
-    response_utils_layer = {
-      path = "layers/response_utils_layer"
-      sources = {
-        source_hash = "response_utils.py"
-      }
-      source_dir = "package"
-    }
-    vector_database_layer = {
-      path = "layers/vector_database_layer"
-      sources = {
-        source_hash = "vector_db_service.py"
-      }
-      source_dir = "package"
-    }
 
-    # Lambda Functions
-    db_bootstrap = {
-      path = "db_bootstrap"
-      sources = {
-        source_hash = "lambda_function.py"
-        schema_hash = "schema.sql"
-      }
-      source_dir = "package"
-    }
-    api_fetch_tasks_handler = {
-      path = "api_fetch_tasks_handler"
-      sources = {
-        source_hash = "lambda_function.py"
-      }
-      source_dir = "package"
-    }
-    api_fetch_videos_handler = {
-      path = "api_fetch_videos_handler"
-      sources = {
-        source_hash = "lambda_function.py"
-      }
-      source_dir = "package"
-    }
-    api_search_handler = {
-      path = "api_search_handler"
-      sources = {
-        lambda_hash     = "lambda_function.py"
-        controller_hash = "search_controller.py"
-        errors_hash     = "search_errors.py"
-      }
-      source_dir = "package"
-    }
-    api_video_upload_link_handler = {
-      path = "api_video_upload_link_handler"
-      sources = {
-        source_hash = "lambda_function.py"
-      }
-      source_dir = "package"
-    }
-    s3_delete_handler = {
-      path = "s3_delete_handler"
-      sources = {
-        lambda_hash = "lambda_function.py"
-        utils_hash  = "utils.py"
-      }
-      source_dir = "package"
-    }
-    sqs_embedding_task_producer = {
-      path = "sqs_embedding_task_producer"
-      sources = {
-        lambda_hash = "lambda_function.py"
-        utils_hash  = "utils.py"
-      }
-      source_dir = "package"
-    }
-    sqs_embedding_task_consumer = {
-      path = "sqs_embedding_task_consumer"
-      sources = {
-        source_hash = "lambda_function.py"
-      }
-      source_dir = "package"
-    }
+resource "null_resource" "build_config_layer" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/layers/config_layer/pyproject.toml")
+    source_hash = filemd5("${local.base_path}/layers/config_layer/config.py")
   }
-}
-
-resource "null_resource" "build_package" {
-  for_each = local.lambda_configs
-
-  triggers = merge(
-    { deps_hash = filemd5("${local.base_path}/${each.value.path}/pyproject.toml") },
-    {
-      for key, file in each.value.sources :
-      key => filemd5("${local.base_path}/${each.value.path}/${file}")
-    }
-  )
 
   provisioner "local-exec" {
-    command     = "./build-package.sh"
-    working_dir = "${local.base_path}/${each.value.path}"
+    command     = local.build_script
+    working_dir = "${local.base_path}/layers/config_layer"
   }
 }
 
-data "archive_file" "packages" {
-  for_each = local.lambda_configs
+resource "null_resource" "build_embed_layer" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/layers/embed_service_layer/pyproject.toml")
+    source_hash = filemd5("${local.base_path}/layers/embed_service_layer/embed_service.py")
+  }
 
+  provisioner "local-exec" {
+    command     = local.build_script
+    working_dir = "${local.base_path}/layers/embed_service_layer"
+  }
+}
+
+resource "null_resource" "build_response_utils_layer" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/layers/response_utils_layer/pyproject.toml")
+    source_hash = filemd5("${local.base_path}/layers/response_utils_layer/response_utils.py")
+  }
+
+  provisioner "local-exec" {
+    command     = local.build_script
+    working_dir = "${local.base_path}/layers/response_utils_layer"
+  }
+}
+
+resource "null_resource" "build_vector_database_layer" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/layers/vector_database_layer/pyproject.toml")
+    source_hash = filemd5("${local.base_path}/layers/vector_database_layer/vector_db_service.py")
+  }
+
+  provisioner "local-exec" {
+    command     = local.build_script
+    working_dir = "${local.base_path}/layers/vector_database_layer"
+  }
+}
+
+resource "null_resource" "build_db_bootstrap" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/db_bootstrap/pyproject.toml")
+    source_hash = filemd5("${local.base_path}/db_bootstrap/lambda_function.py")
+    schema_hash = filemd5("${local.base_path}/db_bootstrap/schema.sql")
+  }
+
+  provisioner "local-exec" {
+    command     = "bash ${local.build_script}"
+    working_dir = "${local.base_path}/db_bootstrap"
+  }
+}
+
+resource "null_resource" "build_api_fetch_tasks_handler" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/api_fetch_tasks_handler/pyproject.toml")
+    source_hash = filemd5("${local.base_path}/api_fetch_tasks_handler/lambda_function.py")
+  }
+
+  provisioner "local-exec" {
+    command     = "bash ${local.build_script}"
+    working_dir = "${local.base_path}/api_fetch_tasks_handler"
+  }
+}
+
+resource "null_resource" "build_api_fetch_videos_handler" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/api_fetch_videos_handler/pyproject.toml")
+    source_hash = filemd5("${local.base_path}/api_fetch_videos_handler/lambda_function.py")
+  }
+
+  provisioner "local-exec" {
+    command     = "bash ${local.build_script}"
+    working_dir = "${local.base_path}/api_fetch_videos_handler"
+  }
+}
+
+resource "null_resource" "build_api_search_handler" {
+  triggers = {
+    deps_hash       = filemd5("${local.base_path}/api_search_handler/pyproject.toml")
+    lambda_hash     = filemd5("${local.base_path}/api_search_handler/lambda_function.py")
+    controller_hash = filemd5("${local.base_path}/api_search_handler/search_controller.py")
+    errors_hash     = filemd5("${local.base_path}/api_search_handler/search_errors.py")
+  }
+
+  provisioner "local-exec" {
+    command     = "bash ${local.build_script}"
+    working_dir = "${local.base_path}/api_search_handler"
+  }
+}
+
+resource "null_resource" "build_api_video_upload_link_handler" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/api_video_upload_link_handler/pyproject.toml")
+    source_hash = filemd5("${local.base_path}/api_video_upload_link_handler/lambda_function.py")
+  }
+
+  provisioner "local-exec" {
+    command     = "bash ${local.build_script}"
+    working_dir = "${local.base_path}/api_video_upload_link_handler"
+  }
+}
+
+resource "null_resource" "build_s3_delete_handler" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/s3_delete_handler/pyproject.toml")
+    lambda_hash = filemd5("${local.base_path}/s3_delete_handler/lambda_function.py")
+    utils_hash  = filemd5("${local.base_path}/s3_delete_handler/utils.py")
+  }
+
+  provisioner "local-exec" {
+    command     = "bash ${local.build_script}"
+    working_dir = "${local.base_path}/s3_delete_handler"
+  }
+}
+
+resource "null_resource" "build_sqs_embedding_task_producer" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/sqs_embedding_task_producer/pyproject.toml")
+    lambda_hash = filemd5("${local.base_path}/sqs_embedding_task_producer/lambda_function.py")
+    utils_hash  = filemd5("${local.base_path}/sqs_embedding_task_producer/utils.py")
+  }
+
+  provisioner "local-exec" {
+    command     = "bash ${local.build_script}"
+    working_dir = "${local.base_path}/sqs_embedding_task_producer"
+  }
+}
+
+resource "null_resource" "build_sqs_embedding_task_consumer" {
+  triggers = {
+    deps_hash   = filemd5("${local.base_path}/sqs_embedding_task_consumer/pyproject.toml")
+    source_hash = filemd5("${local.base_path}/sqs_embedding_task_consumer/lambda_function.py")
+  }
+
+  provisioner "local-exec" {
+    command     = "bash ${local.build_script}"
+    working_dir = "${local.base_path}/sqs_embedding_task_consumer"
+  }
+}
+
+# Lambda Layers
+data "archive_file" "config_layer" {
   type        = "zip"
-  output_path = "${local.base_path}/${each.value.path}/package.zip"
-  source_dir  = "${local.base_path}/${each.value.path}/${each.value.source_dir}"
+  output_path = "${local.base_path}/layers/config_layer/package.zip"
+  source_dir  = "${local.base_path}/layers/config_layer/package"
   excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_config_layer]
+}
+
+data "archive_file" "embed_layer" {
+  type        = "zip"
+  output_path = "${local.base_path}/layers/embed_service_layer/package.zip"
+  source_dir  = "${local.base_path}/layers/embed_service_layer/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_embed_layer]
+}
+
+data "archive_file" "response_utils_layer" {
+  type        = "zip"
+  output_path = "${local.base_path}/layers/response_utils_layer/package.zip"
+  source_dir  = "${local.base_path}/layers/response_utils_layer/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_response_utils_layer]
+}
+
+data "archive_file" "vector_database_layer" {
+  type        = "zip"
+  output_path = "${local.base_path}/layers/vector_database_layer/package.zip"
+  source_dir  = "${local.base_path}/layers/vector_database_layer/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_vector_database_layer]
+}
+
+# Lambda Functions
+data "archive_file" "db_bootstrap" {
+  type        = "zip"
+  output_path = "${local.base_path}/db_bootstrap/package.zip"
+  source_dir  = "${local.base_path}/db_bootstrap/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_db_bootstrap]
+}
+
+data "archive_file" "api_fetch_tasks_handler" {
+  type        = "zip"
+  output_path = "${local.base_path}/api_fetch_tasks_handler/package.zip"
+  source_dir  = "${local.base_path}/api_fetch_tasks_handler/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_api_fetch_tasks_handler]
+}
+
+data "archive_file" "api_fetch_videos_handler" {
+  type        = "zip"
+  output_path = "${local.base_path}/api_fetch_videos_handler/package.zip"
+  source_dir  = "${local.base_path}/api_fetch_videos_handler/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_api_fetch_videos_handler]
+}
+
+data "archive_file" "api_search_handler" {
+  type        = "zip"
+  output_path = "${local.base_path}/api_search_handler/package.zip"
+  source_dir  = "${local.base_path}/api_search_handler/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_api_search_handler]
+}
+
+data "archive_file" "api_video_upload_link_handler" {
+  type        = "zip"
+  output_path = "${local.base_path}/api_video_upload_link_handler/package.zip"
+  source_dir  = "${local.base_path}/api_video_upload_link_handler/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_api_video_upload_link_handler]
+}
+
+data "archive_file" "s3_delete_handler" {
+  type        = "zip"
+  output_path = "${local.base_path}/s3_delete_handler/package.zip"
+  source_dir  = "${local.base_path}/s3_delete_handler/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_s3_delete_handler]
+}
+
+data "archive_file" "sqs_embedding_task_producer" {
+  type        = "zip"
+  output_path = "${local.base_path}/sqs_embedding_task_producer/package.zip"
+  source_dir  = "${local.base_path}/sqs_embedding_task_producer/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_sqs_embedding_task_producer]
+}
+
+data "archive_file" "sqs_embedding_task_consumer" {
+  type        = "zip"
+  output_path = "${local.base_path}/sqs_embedding_task_consumer/package.zip"
+  source_dir  = "${local.base_path}/sqs_embedding_task_consumer/package"
+  excludes    = ["__pycache__", "*.pyc", "*.DS_Store"]
+
+  depends_on = [null_resource.build_sqs_embedding_task_consumer]
 }
 
