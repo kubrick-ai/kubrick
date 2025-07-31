0b48cbf1619657120e1aeabe73a9f0bf46c130c5
Dominique Morales Perez <dmp@Dominiques-MacBook-Pro.local>
2025-07-30 18:52:03 -0700

    Feat: Cloudfront + Public S3 to host NextJS Application

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,6 @@ variable "aws_region" {
 variable "secrets_manager_name" {
   description = "The name of the AWS Secrets Manager secret containing API keys and RDS credentials"
   type        = string
-  default     = "KubrickEmbeddingTaskConsumerSecret"
-  # default     = "KubrickEmbeddingTaskConsumerSecret" ## Changed to try to make testing work, delete this
+  default     = "kubrick_secret"
 }
 
