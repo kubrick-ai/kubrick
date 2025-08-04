0d5f0a42a5e2031be6370b826b3e2a9e7dae344d
Daniel Chae <daniel.jiwoong@gmail.com>
2025-08-04 15:06:03 +0900

    chore: moved lambda source files to `lambda/src` dir
    - build script was building tests into packages
    - this change is to prevent that

--- terraform/modules/lambda/locals.tf
+++ terraform/modules/lambda/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  base_path = abspath("${path.root}/../lambda")
+  base_path    = abspath("${path.root}/../lambda/src")
   build_script = abspath("${path.root}/../lambda/build-package.sh")
 }
