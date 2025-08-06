1b7255751b7d677c4953046af447d2e33281b3e4
Daniel Chae <daniel.jiwoong@gmail.com>
2025-08-07 08:59:15 +0900

    fix: auto-fill grid
    - prevents super large thumbnails when there are only a few

--- playground/src/components/SearchResultList/SearchResultList.tsx
+++ playground/src/components/SearchResultList/SearchResultList.tsx
@@ -24,7 +24,7 @@ const SearchResultList = ({ results }: SearchResultListProps) => {
 
   return (
     <>
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4 ">
         {currentResults.map((result) => (
           <VideoThumbnail
             key={result.id}
